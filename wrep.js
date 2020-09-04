23d819343e43acc5254b7e0c258d0fb0bb835ca1
xv <ihalx@mail.ru>
2020-09-04 17:08:00 +0300

    Скорректированы возвращаемые промисы

--- wrep.js
+++ wrep.js
@@ -1,34 +1,34 @@
-// import express from 'express';
-let express = require('express');
+// import express from "express";
+let express = require("express");
 let app = express();
 
-app.set('port', process.env.PORT || 1059);
-const port = app.get('port');
+app.set("port", process.env.PORT || 1059);
+const port = app.get("port");
 
-// import { login } from './lib/wialonRequest.js';
-let wialon = require('./lib/wialonRequest.js');
+// import { login } from "./lib/wialonRequest.js";
+let wialon = require("./lib/wialonRequest.js");
 
-app.get('/', (req, res) => {
-    let intro = {
-        resourceName: 'ЗАО Октябрьское',
+app.get("/", (req, res) => {
+    let input = {
+        resourceName: "ЗАО Октябрьское",
         reps: [
             {
-                templateName: 'Нормы и пробег',
-                unitGroups: ['Малая техника', 'Специалисты'],
-                outputFileName: '[Малая_техника_Специалисты]_Нормы_и_пробег'
+                templateName: "Нормы и пробег",
+                unitGroups: ["Малая техника", "Специалисты"],
+                outputFileName: "[Малая_техника_Специалисты]_Нормы_и_пробег"
             },
             {
-                templateName: 'Топливо и пробег',
-                unitGroups: ['Камазы', 'Кировцы', 'Комбайны'],
-                outputFileName: '[Камазы_Кировцы_Комбайны]_Топливо_и_пробег'
+                templateName: "Топливо и пробег",
+                unitGroups: ["Камазы", "Кировцы", "Комбайны"],
+                outputFileName: "[Камазы_Кировцы_Комбайны]_Топливо_и_пробег"
             }
         ]
     };
-    wialon.get(intro);
-    res.send('get templates');
+    wialon.get(input);
+    res.send("get templates");
 });
 
 
 app.listen(port, () => {
-    console.log(`WREP-SERVICE is running in ${app.get('env')} mode on http://localhost:${port}`);
+    console.log(`WREP-SERVICE is running in ${app.get("env")} mode on http://localhost:${port}`);
 });
