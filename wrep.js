106228db4da54f09e72c08535f5242568d94e9b3
xv <ihalx@mail.ru>
2020-09-25 18:04:39 +0300

    Реализовано формирование отчётов за месяц.

--- wrep.js
+++ wrep.js
@@ -8,6 +8,11 @@ const port = app.get("port");
 // import { login } from "./lib/wialonRequest.js";
 let wialon = require("./lib/wialonRequest.js");
 
+globalThis.PeriodType = Object.freeze({
+    decade: 0,
+    month: 1
+});
+
 app.get("/", (req, res) => {
     let input = {
         resourceName: "ЗАО Октябрьское",
@@ -15,12 +20,26 @@ app.get("/", (req, res) => {
             {
                 templateName: "Нормы и пробег",
                 unitGroups: ["Малая техника", "Специалисты"],
-                outputFileName: "[Малая_техника_Специалисты]_Нормы_и_пробег"
+                outputFileName: "[Малая_техника_Специалисты]_Нормы_и_пробег",
+                periodType: globalThis.PeriodType.decade
+            },
+            {
+                templateName: "Нормы и пробег",
+                unitGroups: ["Малая техника", "Специалисты"],
+                outputFileName: "[Малая_техника_Специалисты]_Нормы_и_пробег",
+                periodType: globalThis.PeriodType.month
             },
             {
                 templateName: "Топливо и пробег",
                 unitGroups: ["Камазы", "Кировцы", "Комбайны"],
-                outputFileName: "[Камазы_Кировцы_Комбайны]_Топливо_и_пробег"
+                outputFileName: "[Камазы_Кировцы_Комбайны]_Топливо_и_пробег",
+                periodType: globalThis.PeriodType.decade
+            },
+            {
+                templateName: "Топливо и пробег",
+                unitGroups: ["Камазы", "Кировцы", "Комбайны"],
+                outputFileName: "[Камазы_Кировцы_Комбайны]_Топливо_и_пробег",
+                periodType: globalThis.PeriodType.month
             }
         ]
     };
